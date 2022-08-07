85b2e168c2c1a12656de98fcb64ac31a0d25c7fa
Susuwatari <caroline.fang.cc@gmail.com>
2022-08-08 00:33:48 +0800

    updata

---
+++ ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x2040d4133f64823f367e0572edee4dccc6b65fbd";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
