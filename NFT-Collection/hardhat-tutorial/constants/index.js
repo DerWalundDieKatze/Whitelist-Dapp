a1d87727f66c3278fcea5bda8d20365fde60934a
Susuwatari <caroline.fang.cc@gmail.com>
2022-08-07 23:06:29 +0800

    updata

--- NFT-Collection/hardhat-tutorial/constants/index.js
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x757ffFd5BbE722bbb7c00b7Ac8BE4189492FBcC5";
+const WHITELIST_CONTRACT_ADDRESS = "0x6786F05B52891CB7185853eF5e64B0D82F9C12eD";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://new-fawn-sigma.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
